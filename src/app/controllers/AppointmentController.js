94912eed475e5720f5277bfc1fe20c2f856a6e03
Gus Prado <contato@gusprado.com>
2019-10-28 11:48:36 -0300

    added cancel appointments and email notifications

--- src/app/controllers/AppointmentController.js
+++ src/app/controllers/AppointmentController.js
@@ -1,10 +1,11 @@
 import * as Yup from 'yup';
-import { startOfHour, parseISO, isBefore, format } from 'date-fns';
+import { startOfHour, parseISO, isBefore, format, subHours } from 'date-fns';
 import pt from 'date-fns/locale/pt';
 import Appointment from '../models/Appointment';
 import Notification from '../schemas/Notification';
 import User from '../models/User';
 import File from '../models/File';
+import Mail from '../../lib/Mail';
 
 class AppointmentController {
     async index(req, res) {
@@ -104,6 +105,45 @@ class AppointmentController {
         await Notification.create({
             content: `Novo agendamento para ${user.name} no dia ${formattedDate}`,
             user: provider_id
+        });
+
+        return res.json(appointment);
+    }
+
+    async delete(req, res) {
+        const appointment = await Appointment.findByPk(req.params.id, {
+            include: [
+                {
+                    model: User,
+                    as: 'provider',
+                    attributes: ['name', 'email']
+                }
+            ]
+        });
+
+        if (appointment.user_id !== req.userId) {
+            return res.status(401)({
+                error: "You don't have permission to cancel this appointment"
+            });
+        }
+
+        const dateWithSub = subHours(appointment.date, 2);
+
+        if (isBefore(dateWithSub, new Date())) {
+            return res.status(401).json({
+                error:
+                    'Cancellations are only accepted at max of 2 hours before event'
+            });
+        }
+
+        appointment.canceled_at = new Date();
+
+        await appointment.save();
+
+        await Mail.sendMail({
+            to: `${appointment.provider.name} <${appointment.provider.email}>`,
+            subject: 'Appointment cancelled',
+            text: 'You have a new appointment cancel'
         });
 
         return res.json(appointment);
---
+++ src/config/mail.js
@@ -0,0 +1,21 @@
+export default {
+    host: 'smtp.mailtrap.io',
+    port: '2525',
+    secure: false,
+    auth: {
+        user: '63028c6b449f64',
+        pass: '9dd4f361f8b24b'
+    },
+    default: {
+        from: 'GoBarber team <noreply@gobarber.com>'
+    }
+};
+
+/*
+Mail Svcs:
+Amazon SES
+Mailgun
+Sparkpost
+Mandril -> Mailchimp
+Mailtrap -> DEV env
+*/
